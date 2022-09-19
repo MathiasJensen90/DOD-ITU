8f6b7eec933358d50eb5bb0c4b4a46180af275a5
darkpanix <38384941+darkpanix@users.noreply.github.com>
2022-09-20 00:08:07 +0200

    More updates

---
+++ DOTS Example Project 0.51/Assets/Scenes/9. IjobEnttiy/Scripts/IJobEntityExample.cs
@@ -0,0 +1,46 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public partial class IJobEntityExample : SystemBase 
+{
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<IJobSingleton>();
+    }
+
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+        
+        RotationJob rotationJob = new RotationJob
+        {
+            deltaTime = deltaTime
+        };
+
+        JobHandle rotationJobhandle = rotationJob.Schedule();
+        
+        rotationJobhandle.Complete();
+
+        #region EFE
+        // Entities.ForEach((ref Rotation rot, in JobentityData jobentityData) =>
+        // {
+        //     rot.Value = math.mul(math.normalize(rot.Value),
+        //         quaternion.AxisAngle(math.up(), math.radians(jobentityData.rotationValue) * deltaTime));
+        // }).Schedule();
+        #endregion
+    }
+}
+
+
+public partial struct RotationJob : IJobEntity
+{
+    public float deltaTime;
+    public void Execute(ref Rotation rot, in JobentityData jobentityData)
+    {
+        rot.Value = math.mul(math.normalize(rot.Value),
+            quaternion.AxisAngle(math.up(), math.radians(jobentityData.rotationValue) * deltaTime));
+    }
+}
