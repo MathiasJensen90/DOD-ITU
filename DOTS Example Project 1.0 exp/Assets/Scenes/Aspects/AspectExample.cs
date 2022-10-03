de3be42348b6b3b8d5265bf6729dfb696c710b47
darkpanix <38384941+darkpanix@users.noreply.github.com>
2022-10-03 21:38:48 +0200

    Initial enable example

--- DOTS Example Project 1.0 exp/Assets/Scenes/Aspects/AspectExample.cs
+++ DOTS Example Project 1.0 exp/Assets/Scenes/Aspects/AspectExample.cs
@@ -1,4 +1,7 @@
-﻿using Unity.Entities;
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 
@@ -6,7 +9,7 @@ public partial struct AspectExampleSystem : ISystem
 {
     public void OnCreate(ref SystemState state)
     {
-        
+        state.RequireForUpdate<moveSpeed>();
     }
 
     public void OnDestroy(ref SystemState state)
@@ -16,6 +19,25 @@ public partial struct AspectExampleSystem : ISystem
 
     public void OnUpdate(ref SystemState state)
     {
-        
+        float elapsedTime = (float)SystemAPI.Time.ElapsedTime;
+
+        foreach (var sin in SystemAPI.Query<SinAspect>())
+        {
+            sin.Move(elapsedTime);
+            
+            
+        }
+    }
+}
+
+
+readonly partial struct SinAspect : IAspect
+{
+    readonly RefRW<LocalToWorldTransform> m_Transform;
+    readonly RefRO<moveSpeed> m_Speed;
+
+    public void Move(double elapsedTime)
+    {
+        m_Transform.ValueRW.Value.Position.y = (float)math.sin(elapsedTime * m_Speed.ValueRO.Value);
     }
 }
---
+++ DOTS Example Project 1.0 exp/Assets/Scenes/IenableComponents/InitSystem.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public partial struct InitSystem : ISystem 
+{
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<StateComp>();
+    }
+
+    public void OnDestroy(ref SystemState state)
+    {
+      
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        var config = SystemAPI.GetSingleton<StateComp>();
+
+        var size = config.size * config.size;
+        var center = (config.size - 1) / 2f;
+
+        for (int i = 0; i < size; i++)
+        {
+            float3 pos = float3.zero;
+            pos.x = (i % config.size- center) * 1.5f;
+            pos.z = (i / config.size - center) * 1.5f;
+            var e = state.EntityManager.Instantiate(config.prefab);
+            state.EntityManager.SetComponentData(e, new LocalToWorldTransform
+            {
+                Value = UniformScaleTransform.FromPosition(pos)
+            });
+        }
+        state.Enabled = false;
+    }
+}
